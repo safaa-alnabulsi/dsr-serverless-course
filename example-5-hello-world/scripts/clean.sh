a2fb31a7163fa3a69c6861767d1884b9a32d72cc
Safaa Alnabulsi <safaa.alnablsi@gmail.com>
2020-03-02 21:03:19 +0100

    Remove the extra cleaning steps for s3 bucket from this example

--- example-5-hello-world/scripts/clean.sh
+++ example-5-hello-world/scripts/clean.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 STACK_ROLE_NAME="hello-world-lambda-iam-role"
-STACK_BUCKET_NAME="polly-audio-bucket-stack"
-BUCKET_NAME="polly-audiofiles-bucket"
 
 # get the value of the first parameter
 FUNCTION_NAME=$1
@@ -12,9 +10,3 @@ aws lambda delete-function --function-name $FUNCTION_NAME
 
 # clean the iam role
 aws cloudformation delete-stack --stack-name $STACK_ROLE_NAME
-
-# empty the s3 bucket
-aws s3 rm s3://$BUCKET_NAME/* --recursive
-
-# clean the s3 bucket
-aws cloudformation delete-stack --stack-name $STACK_BUCKET_NAME
