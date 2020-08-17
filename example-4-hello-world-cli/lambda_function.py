96ca0288ac17392991b3f167e67b9da40f3d355f
Safaa Alnabulsi <safaa.alnablsi@gmail.com>
2020-08-17 15:01:05 +0200

    Add a helping function inside the lambda ex04

--- example-4-hello-world-cli/lambda_function.py
+++ example-4-hello-world-cli/lambda_function.py
@@ -1,6 +1,9 @@
 
 def lambda_handler(event, context):
-	print('inside the lambda function')
-	# print("value1 = ",  event['key1'])
-	# return event['key1']
-	return event
+	print('inside the lambda function cli')
+	print("value1 = ",  event['key1'])
+	write_my_name('Nina')
+	return event['key1']
+
+def write_my_name(name):
+	print("Hello from inside write_by_name function", name)
