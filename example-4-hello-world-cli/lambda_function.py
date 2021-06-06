df7cbc94ab174fb6c951c2c4f47f1216fd92a585
Safaa Alnabulsi <safaa.alnablsi@gmail.com>
2021-06-06 20:35:19 +0200

    Remove some code

--- example-4-hello-world-cli/lambda_function.py
+++ example-4-hello-world-cli/lambda_function.py
@@ -2,8 +2,4 @@
 def lambda_handler(event, context):
 	print('inside the lambda function cli')
 	print("value1 = ",  event['key1'])
-	write_my_name('Nina')
 	return event['key1']
-
-def write_my_name(name):
-	print("Hello from inside write_by_name function", name)
--- example-5-hello-world-scripts/lambda_function.py
+++ example-5-hello-world-scripts/lambda_function.py
@@ -2,9 +2,4 @@
 def lambda_handler(event, context):
 	print('inside the lambda function')
 	print("value1 = ",  event['key1'])
-	print('updated lambda from shell script')
-	print(my_sum(1, 2))
 	return event['key1']
-
-def my_sum(a,b):
-	return a + b
